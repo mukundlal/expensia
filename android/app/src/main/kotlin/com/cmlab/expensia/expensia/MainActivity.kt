a259c8e5828f0e82c51fd8f424ebe5281ff0dbb9
Mukund Lal <mukundlal@Mukunds-MacBook-Air.local>
2022-04-18 08:43:16 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/cmlab/expensia/expensia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cmlab.expensia.expensia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/widgets/credit_item.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class CreditItemWidget extends StatelessWidget {
+  const CreditItemWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
